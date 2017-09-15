7dbd4864d78ff3258f0a64ddd0d4fa71aba64d2d
zhuhuishao <zhuhuishao@itouchtv.cn>
2017-09-15 11:20:34 +0800

    project init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Zepto from 'n-zepto'
 import {HMAC_SHA256_init, HMAC_SHA256_write, HMAC_SHA256_finalize} from './sha256'
+import { doEncode, doDecode } from './base64';
 
 //md5算法
 function getMD5(encryptString) {
